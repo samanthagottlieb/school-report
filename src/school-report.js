31650535aa0360a6e3733fe51bb3dd145bd8c1c2
Samantha <samanthagottlieb31@gmail.com>
2021-03-16 11:35:34 +0000

    Returns one red or one amber score - hard coded

--- src/school-report.js
+++ src/school-report.js
@@ -4,7 +4,11 @@ class SchoolReport {
   }
 
   createReport() {
-    return "Red: 1";
+    if (this.results == "0") {
+      return "Red: 1";
+    } else {
+      return "Amber: 1";
+    }
   }
 }
 
--- test/school-report.test.js
+++ test/school-report.test.js
@@ -2,9 +2,14 @@ const SchoolReport = require("../src/school-report");
 
 describe("SchoolReport", () => {
   describe("createReport", () => {
-    it("counts one test score", () => {
+    it("counts one red test score", () => {
       let report = new SchoolReport("0");
       expect(report.createReport()).toEqual("Red: 1");
+    });
+
+    it("counts one amber test score", () => {
+      let report = new SchoolReport("50");
+      expect(report.createReport()).toEqual("Amber: 1");
     });
   });
 });
